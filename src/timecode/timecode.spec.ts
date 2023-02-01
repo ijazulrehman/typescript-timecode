dec3cade98183d8e242ecc7debfc59b561b325a8
Ijaz Ul Rehman <ijazulrehmannajeeb@gmail.com>
2023-02-01 18:28:37 +0500

    fix: ms concatination

--- src/timecode/timecode.spec.ts
+++ src/timecode/timecode.spec.ts
@@ -6,7 +6,12 @@ describe("Timecode test", () => {
     expect(res).toBe("05:20");
   });
 
-  test("", () => {
+  test("Converts millisecond to standard time code", () => {
+    const res = Timecode.MillisecondsToTime(320009, ".");
+    expect(res).toBe("05:20.009");
+  });
+
+  test("Converts millisecond to standard time code", () => {
     const res = Timecode.TimetoMilliseconds("05:20");
     expect(res).toBe(320000);
   });
@@ -18,6 +23,16 @@ describe("Timecode test", () => {
 
   test("Converts millisecond to HH:MM:ss.SSS format timecode", () => {
     const res = Timecode.MillisecondsToHHMMssSSS(320009);
-    expect(res).toBe("00:05:20.90");
+    expect(res).toBe("00:05:20.009");
+  });
+
+  test("Converts millisecond to HH:MM:ss,SSS format timecode", () => {
+    const res = Timecode.MillisecondsToHHMMssSSS(320009, ",");
+    expect(res).toBe("00:05:20,009");
+  });
+
+  test("Converts millisecond to HH:MM:ss:SSS format timecode", () => {
+    const res = Timecode.MillisecondsToHHMMssSSS(320009, ":");
+    expect(res).toBe("00:05:20:009");
   });
 });
